e733ddcaed22dd40b9a2d5b3966d1e19bf3b7424
chenjuncheng00 <chenjuncheng888@gmail.com>
2020-06-12 20:24:47 +0800

    update

--- 燃气轮机分布式能源负荷分析计算程序/My Project/AssemblyInfo.vb
+++ 燃气轮机分布式能源负荷分析计算程序/My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("燃气轮机分布式能源负荷分析计算程序")>
 <Assembly: AssemblyCompany("上海发电设备成套设计研究院")>
 <Assembly: AssemblyProduct("燃气轮机分布式能源负荷分析计算程序")>
-<Assembly: AssemblyCopyright("Copyright © 陈俊丞 2016-2020")>
+<Assembly: AssemblyCopyright("Copyright © 陈俊丞 SINCE 2016")>
 <Assembly: AssemblyTrademark("SPERI")>
 <Assembly: ComVisible(True)>
 '如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
